31610d7cb3f958eec08254e74b93beed8a4f00e1
joy061211 <79034483+joy061211@users.noreply.github.com>
2021-02-21 18:59:39 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TanHalfPi",
+    version='1.0.0',
+    description='TanHalfPi is large num type.',
+    author='NP Developer Team',
+    author_email='joylove0612@naver.com',
+    url='github.com/Plel-Coder/NPModulePackage',
+    pymodules=["TanHalfPi"]
+)
